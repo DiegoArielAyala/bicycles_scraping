136ca45008350204a35acbaf56159a3e1543697a
DiegoArielAyala <diegoa.ayala@gmail.com>
2025-07-05 15:30:44 +0200

    Add debug logging

--- scraping/views.py
+++ scraping/views.py
@@ -99,7 +99,9 @@ def run_scraper(start_page, last_page):
     counter = int(start_page)
     while not usp_warn and counter <= last_page:
         print(f"Get request page {counter}")
+        print(urljoin(bicycles_url, page_endpoint.format(counter)))
         response = requests.get(urljoin(bicycles_url, page_endpoint.format(counter)))
+        print(response.status_code)
         if (
             "No podemos encontrar productos que coincida con la selección."
             in response.text
