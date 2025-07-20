1bf3bf9b2a897ab4d58bd3b6d9e076b529079360
DiegoArielAyala <diegoa.ayala@gmail.com>
2025-07-20 16:43:09 +0200

    Remove django_extensions from installed_apps

--- bicyclesscraping/settings.py
+++ bicyclesscraping/settings.py
@@ -45,8 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "scraping",
-    "django_extensions"
+    "scraping"
 ]
 
 MIDDLEWARE = [
