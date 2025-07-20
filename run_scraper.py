f4b13996e09fae8a9d8484a4c962b77b15509fe2
DiegoArielAyala <diegoa.ayala@gmail.com>
2025-07-20 13:01:54 +0200

    Init Django before exec scraper

--- run_scraper.py
+++ run_scraper.py
@@ -1,5 +1,14 @@
+import os
+import django
 import asyncio
-from scraping.views import run_scraper
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bicyclesscraping.settings")
+django.setup()
+
+from scraping.views import run_scraper 
 
 def run():
-    asyncio.run(run_scraper(1, 30)) 
+    asyncio.run(run_scraper(1, 30))
+
+if __name__ == "__main__":
+    run()
