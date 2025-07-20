5a1b2291d3c0cd7699de97c2b13df83c087c5c6d
DiegoArielAyala <diegoa.ayala@gmail.com>
2025-07-20 18:08:58 +0200

    Change run_scraper to set start and end pages for scraping

--- run_scraper.py
+++ run_scraper.py
@@ -1,14 +1,20 @@
 import os
 import django
 import asyncio
+import argparse
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bicyclesscraping.settings")
 django.setup()
 
-from scraping.views import run_scraper 
+from scraping.views import run_scraper
 
-def run():
-    asyncio.run(run_scraper(1, 30))
+def main():
+    parser = argparse.ArgumentParser(description="Run scraper with page range")
+    parser.add_argument("--start", type=int, default=1, help="Start page")
+    parser.add_argument("--end", type=int, default=30, help="End page")
+    args = parser.parse_args()
+
+    asyncio.run(run_scraper(args.start, args.end))
 
 if __name__ == "__main__":
-    run()
+    main()
