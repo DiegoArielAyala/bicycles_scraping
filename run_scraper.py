da426ac0467bd28bd4ee11251e804b45fc65a2a3
DiegoArielAyala <diegoa.ayala@gmail.com>
2025-07-29 14:14:46 +0200

    New workflow for scraping

--- run_scraper.py
+++ run_scraper.py
@@ -11,10 +11,11 @@ from scraping.views import run_scraper
 def main():
     parser = argparse.ArgumentParser(description="Run scraper with page range")
     parser.add_argument("--start", type=int, default=1, help="Start page")
-    parser.add_argument("--end", type=int, default=30, help="End page")
+    parser.add_argument("--end", type=int, default=25, help="End page")
+    parser.add_argument("--web", type=str, default=None, help="Shop name")
     args = parser.parse_args()
 
-    asyncio.run(run_scraper(args.start, args.end))
+    asyncio.run(run_scraper(args.start, args.end, args.web))
 
 if __name__ == "__main__":
     main()
