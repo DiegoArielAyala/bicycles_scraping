df4dec1bd7898e4407ca2555c22fd68ee188c02d
DiegoArielAyala <diegoa.ayala@gmail.com>
2026-02-11 09:03:26 +0100

    WIP

--- scraping/views.py
+++ scraping/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
-from bs4 import BeautifulSoup
-import os, dotenv
+import asyncio 
+import os
+import dotenv
 import json
 import smtplib
 from email.mime.text import MIMEText
@@ -13,44 +14,15 @@ from django.contrib.auth import login, authenticate, logout
 from django.db import IntegrityError
 from .models import Bicycle, PriceHistory, Subscription
 from .forms import SubscriptionForm
-from .utils import create_bicycles
 from django.http import JsonResponse
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib import messages
-from bs4 import BeautifulSoup
-from playwright.async_api import async_playwright
-from playwright_stealth.stealth import Stealth
-import asyncio, random
-import re
-from asgiref.sync import sync_to_async
 from django.core.exceptions import ObjectDoesNotExist
+from .scraper import run_scraper
 
 
 dotenv.load_dotenv(".env." + os.getenv("ENV", "local"))
-
-urls = {
-    "escapa": {
-        "bicycles_endpoint": "https://www.biciescapa.com/es/bicicletas/?en-stock=1&page={}",
-        "web": "https://www.biciescapa.com/es/"
-    },
-    "biking_point": {
-        "bicycles_endpoint": "https://www.bikingpoint.es/es/bicicletas.html/?p={}",
-        "search_endpoint": "https://www.bikingpoint.es/es/catalogsearch/result/?q={}",
-    }
-}
-
-USER_AGENTS = [
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.199 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.183",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Safari/605.1.15",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 12_6_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.198 Safari/537.36",
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
-    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:112.0) Gecko/20100101 Firefox/112.0",
-    "Mozilla/5.0 (Linux; Android 11; SM-A715F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Mobile Safari/537.36",
-    "Mozilla/5.0 (iPhone; CPU iPhone OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Mobile/15E148 Safari/604.1",
-    "Mozilla/5.0 (iPad; CPU OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Mobile/15E148 Safari/604.1"
-]
 
 
 def home(request):
@@ -148,155 +120,6 @@ def extract_bicycles_from_web(request, start_page=1, last_page=30):
 
     return JsonResponse({"message": "Scraping started in background"})
 
-
-async def run_scraper(start_page, last_page, web=None, delete=False):
-    print("run_scraper function start")
-    async with async_playwright() as p:
-        browser = await p.chromium.launch(
-            headless=True, args=["--no-sandbox", "--disable-dev-shm-usage"]
-        )
-        counter = int(start_page)
-
-        # Array with all bicycles references in the DB for the current web
-        bicycle_references = await sync_to_async(
-            lambda: list(Bicycle.objects.filter(web=web).values_list("reference", flat=True,))
-            )()
-
-        while counter <= last_page:
-
-            url = (urls[web]["bicycles_endpoint"]).format(counter)
-            print(f"url: {url}")
-            try:
-                list_page = await browser.new_page(user_agent=random.choice(USER_AGENTS))
-                
-                stealth = Stealth()
-                await stealth.apply_stealth_async(list_page)
-                await asyncio.sleep(random.uniform(3, 6))
-                await list_page.goto(url, wait_until="domcontentloaded")
-                html = await list_page.content()
-                soup = BeautifulSoup(html, "html.parser")
-                try:
-                    await list_page.wait_for_function("() => !document.body.innerText.includes('Verifying you are human')", timeout=180000)
-                except:
-                    print(f"Error cloudflare challenge no se resolvio")
-
-                html = await list_page.content()
-                soup = BeautifulSoup(html, "html.parser")
-
-                if web == "biking_point":
-                    if (
-                        "No podemos encontrar productos que coincida con la selección."
-                        in soup.text
-                    ):
-                        print("No hay más productos, finalizando.")
-                        break
-                    else:
-                        bicycles = soup.find_all("li", class_="item product product-item")
-                        print(f"Página {counter}: Encontradas {len(bicycles)} bicicletas")
-                
-                if web == "escapa":
-                    bicycles = soup.find_all("article", class_="product-miniature js-product-miniature mb-3")
-                                
-                # Call to create_bicycles and return an arrays with referencies that not exist yet in the DB
-                bicycle_references = await create_bicycles(bicycles, USER_AGENTS, web, bicycle_references)
-                print(f"bicycle_references: {bicycle_references}")
-                
-                counter += 1
-
-                if web == "escapa":
-                    search_number = (re.search(r"Mostrando \d+-(\d+)", soup.text)).group(1)
-                    number_bicycles = (re.search(r"de (\d+) producto", soup.text)).group(1)
-                    print(search_number)
-                    print(number_bicycles)
-                    if (number_bicycles == search_number):
-                        print("No hay más productos, finalizando.")
-                        break
-
-            except Exception as e:
-                print(f"Error en la página {counter}: {e}")
-                break
-        
-        # Delete bicycles that no longer exists
-        if delete:
-            await delete_bicycles(bicycle_references)
-
-
-        await browser.close()
-    print("Scraping terminado.")
-
-
-async def delete_bicycles(bicycles_reference):
-    print("Deleting bicycles")
-    for reference in bicycles_reference:
-        async with async_playwright() as p:
-            browser = await p.chromium.launch(
-                headless=True, args=["--no-sandbox", "--disable-dev-shm-usage"]
-            )
-            try:
-                page = await browser.new_page(user_agent=random.choice(USER_AGENTS))
-                stealth = Stealth()
-                await stealth.apply_stealth_async(page)
-                await asyncio.sleep(random.uniform(3, 6))
-                bicycle = await sync_to_async(lambda: get_object_or_404(Bicycle, reference=reference))()
-
-                # Search reference on the corresponding page
-                bicycle_exist = True
-                if bicycle.web == "biking_point":
-                    url = urls["biking_point"]["search_endpoint"]
-                    await page.goto(url.format(reference))
-                    content = await page.content()
-                    if "La búsqueda no ha devuelto ningún resultado." in content:
-                        bicycle_exist = False
-
-                elif bicycle.web == "escapa":
-                    url = urls["escapa"]["web"]
-                    await page.goto(url)
-                    try:
-                        await page.click("button#onetrust-accept-btn-handler", timeout=3000)
-                    except:
-                        pass
-
-                    await page.fill("input[name='s']", str(reference))
-                    await asyncio.sleep(random.uniform(3, 6))
-                    await page.wait_for_selector("div.dfd-card-flag", timeout=5000)
-
-                    content = await page.content()
-                    soup = BeautifulSoup(content, "html.parser")
-                    try:
-                        div = soup.find("div", class_="dfd-card-flag", attrs={"data-availability":"out-of-stock"})
-                    
-                        if div.text.strip() == "Agotado" or "Prueba de nuevo con otra búsqueda…" in soup.text:
-                            bicycle_exist = False
-                    except Exception as e:
-                        print(f"Error finding bicycle in Escapa: {e}")
-
-                # If bicycle not exist, delete it
-                if not bicycle_exist:
-                    await sync_to_async(bicycle.delete)()
-                    print(f"Reference {reference} was deleted from web {bicycle.web}")
-
-            except Exception as e:
-                print("Error during delete bicycle: ", e)
-            
-            finally:
-                await browser.close()
-
-"""
-def search_bicycle(request, query=None):
-    if request.method == "GET":
-        return render(request, "search_bicycle.html")
-    else:
-        query = request.POST["query"]
-        try:
-            reference = int(query)
-            if len(query) == 5:
-                results = Bicycle.objects.get(reference=reference)
-            else:
-                results = Bicycle.objects.filter(name__icontains=query)
-        except:
-            results = Bicycle.objects.filter(name__icontains=query)
-        return render(request, "search_bicycle.html", {"results": results})
-"""
 def search_bicycle(request):
     results = []
     max_results = 50  # límite para no sobrecargar workers
