50ea432c34e695058c28277b1006961331356052
DiegoArielAyala <diegoa.ayala@gmail.com>
2026-05-20 07:48:16 +0200

    update static file path in settings and log errors in cron files

--- config/settings.py
+++ config/settings.py
@@ -159,7 +159,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "apps/scraping/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = [
